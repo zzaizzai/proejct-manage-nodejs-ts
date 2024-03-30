dfa135544611b9d331062184564488edbcc448f6
zzaizzai <junsai0530@gmail.com>
2024-03-30 20:12:15 +0900

    init

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.listen(8080, function() {
+    console.log('listening on 8080')
+    
+});
+
+app.get('/pet', function(req: any, res: any){
+    res.send('hello')
+})
+
+
+
+
+
