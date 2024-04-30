17ccb149994bed523c7858d33882ee1d0cff82ec
zzaizzai <junsai0530@gmail.com>
2024-05-01 02:02:10 +0900

    show results in project detail

---
+++ public/js/detail_api.js
@@ -0,0 +1,69 @@
+
+function changeTaskCloseStateApi(button) {
+    const taskId = button.closest('.task-item').dataset.taskId;
+    const stateClosed = button.closest('.task-item').dataset.stateClosed
+    fetch(`/api/tasks/change_close_state`, {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            taskId: taskId,
+            stateClosed: stateClosed
+        })
+    })
+    .then(response => {
+        if (!response.ok) {
+        throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data.data);
+        window.location.reload()
+    })
+    .catch(error => {
+        console.error('Error fetching tasks:', error);
+    });
+}
+
+function getAllTasksWithParentProjectIdApi(button) {
+
+    const taskId = button.closest('.task-item').dataset.taskId;
+    const taskListOfResult = button.closest('.task-item').querySelector('.list-group-item');
+
+    // hide if show already
+    if (taskListOfResult) {
+        taskListOfResult.remove();
+        return
+    }
+
+    fetch(`/api/get_all_results_with_parent_task_id?parent_project_id=${taskId}`)
+    .then(response => {
+        if (!response.ok) {
+        throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data.tasks);
+
+        data.tasks.forEach(task => {
+            const listItem = document.createElement('li');
+            listItem.classList.add('list-group-item');
+            listItem.textContent = task.name; // 또는 task.description 등의 적절한 데이터를 사용합니다.
+            
+
+            // remove dupl
+            if (taskListOfResult) {
+                taskListOfResult.remove();
+            }
+
+            button.closest('.task-item').appendChild(listItem);
+        });
+
+    })
+    .catch(error => {
+        console.error('Error fetching tasks:', error);
+    });
+}
